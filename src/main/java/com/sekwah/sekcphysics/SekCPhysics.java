8b6a6c8df23bef307497d6c7c65d2e857cc085c1
Sekwah <sekwah41@gmail.com>
2018-08-22 04:23:21 +0100

    Added more comments

--- src/main/java/com/sekwah/sekcphysics/SekCPhysics.java
+++ src/main/java/com/sekwah/sekcphysics/SekCPhysics.java
@@ -6,9 +6,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dimdev.rift.listener.MinecraftStartListener;
 
-// To see available listeners https://github.com/DimensionalDevelopment/Rift/tree/master/src/main/java/org/dimdev/rift/listener
+/**
+ * To see available listeners https://github.com/DimensionalDevelopment/Rift/tree/master/src/main/java/org/dimdev/rift/listener
+ * or
+ * {@link org.dimdev.rift.listener}
+  */
 @SuppressWarnings("unused")
-public class SekCPhysics implements MinecraftStartListener {
+public class SekCPhysics implements   {
 
     public static final Logger logger = LogManager.getLogger("SekC Physics");
 
