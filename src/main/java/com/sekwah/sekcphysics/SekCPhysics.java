4a8b4ab07ca4cc2b38b52c14b58ddc2ca80f59b8
Sekwah <sekwah41@gmail.com>
2018-08-23 03:04:31 +0100

    Looking at registering entities

--- src/main/java/com/sekwah/sekcphysics/SekCPhysics.java
+++ src/main/java/com/sekwah/sekcphysics/SekCPhysics.java
@@ -2,12 +2,20 @@ package com.sekwah.sekcphysics;
 
 import com.sekwah.sekcphysics.ragdoll.Ragdolls;
 import com.sekwah.sekcphysics.ragdoll.generation.RagdollGenerator;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.dimdev.rift.listener.EntityTypeAdder;
 import org.dimdev.rift.listener.MinecraftStartListener;
+import org.dimdev.rift.listener.client.EntityRendererAdder;
 import org.dimdev.riftloader.listener.InitializationListener;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
+
+import java.util.Map;
 
 /**
  * To see available listeners https://github.com/DimensionalDevelopment/Rift/tree/master/src/main/java/org/dimdev/rift/listener
@@ -15,7 +23,7 @@ import org.spongepowered.asm.mixin.Mixins;
  * {@link org.dimdev.rift.listener}
   */
 @SuppressWarnings("unused")
-public class SekCPhysics implements MinecraftStartListener {
+public class SekCPhysics implements MinecraftStartListener, EntityTypeAdder, EntityRendererAdder {
 
     public static final Logger logger = LogManager.getLogger("SekC Physics");
 
@@ -33,4 +41,13 @@ public class SekCPhysics implements MinecraftStartListener {
         new RagdollGenerator().loadRagdolls();
     }
 
+    @Override
+    public void registerEntityTypes() {
+        EntityType.register("sekcphysics:ragdoll", EntityType.Builder.create())
+    }
+
+    @Override
+    public void addEntityRenderers(Map<Class<? extends Entity>, Render<? extends Entity>> entityRenderMap, RenderManager renderManager) {
+        
+    }
 }
