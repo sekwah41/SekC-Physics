9f1c4c5a2c0055b397ab993394b0ff68741704bd
Sekwah <sekwah41@gmail.com>
2021-03-23 00:49:56 +0000

    Resetting to code from scratch for 1.16
    Preparing to start coding again as both the fabric and forge versions had different features

--- src/main/java/com/sekwah/sekcphysics/SekCPhysics.java
+++ src/main/java/com/sekwah/sekcphysics/SekCPhysics.java
@@ -1,11 +1,7 @@
 package com.sekwah.sekcphysics;
 
 import com.sekwah.sekcphysics.config.RagdollConfig;
-import com.sekwah.sekcphysics.ragdoll.Ragdolls;
-import com.sekwah.sekcphysics.ragdoll.ragdolls.vanilla.VanillaRagdolls;
-import com.sekwah.sekcphysics.util.Reflection;
 import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -17,38 +13,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-
-/**
- * Created by sekwah on 31/07/2015.
- * Atm the mod will just be client side which will stop the server which will probably be quite slow doing all the calculations
- *  also the players have complete controll over them, after all at least the ragdolls are supposed to only be for visual
- *   effects :3
- *
- *   Dont need to use proxy but do just in case you start adding server side too stuff for non visual things.
- *   Also can add something to add warnings to the server console(itll also stop crashes on a server if someone doesnt pay
- *   attention)
- *
- *   http://wiki.lwjgl.org/wiki/OpenCL_in_LWJGL.html
- *
- *   lwjgl has opencl built in, take a look for future reference :D
- *
- *   Also take a look at maybe making the ragdolls not based on entities and soley their own thing to stop strange interactions.
- *  (For now use entities)
- *
- *   Also potentially add physics to the cloak, even if its as a giant square, but maybe split to small blocks and do it like
- *   real cloth
- */
 @Mod(SekCPhysics.MODID)
 public class SekCPhysics {
 
     public static final String MODID = "sekcphysics";
 
     public static final Logger LOGGER = LogManager.getLogger("SekC Physics");
-
-    public static Reflection REFLECTION = new Reflection();
-
-    public static Ragdolls RAGDOLLS = new Ragdolls();
 
     public SekCPhysics() {
 
@@ -65,8 +35,6 @@ public class SekCPhysics {
     private void clientSetup(final FMLClientSetupEvent event) {
 
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        VanillaRagdolls.register();
 
     }
 
--- src/main/java/com/sekwah/sekcphysics/config/RagdollConfig.java
+++ src/main/java/com/sekwah/sekcphysics/config/RagdollConfig.java
@@ -1,7 +1,9 @@
 package com.sekwah.sekcphysics.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod.EventBusSubscriber
 public class RagdollConfig {
@@ -10,33 +12,40 @@ public class RagdollConfig {
 
     public static ForgeConfigSpec RAGDOLL_CONFIG;
 
-    public static ForgeConfigSpec.ConfigValue<Integer> MAX_RAGDOLLS;
+    public static ForgeConfigSpec.ConfigValue<Integer> CONFIG_MAX_RAGDOLLS;
+    public static int MAX_RAGDOLLS;
 
-    public static ForgeConfigSpec.ConfigValue<Integer> RAGDOLL_LIFE;
+    public static ForgeConfigSpec.ConfigValue<Integer> CONFIG_RAGDOLL_LIFE;
+    public static int RAGDOLL_LIFE;
 
     static {
         ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
 
         CLIENT_BUILDER.comment("Server side variable allowing transparent skins to be set.").push(CATEGORY_CLIENT);
 
-        MAX_RAGDOLLS = CLIENT_BUILDER.comment("How many should be the max number able to exist at once? -1 = unlimited")
-                .define("maxRagdolls ", -1);
+        CONFIG_MAX_RAGDOLLS = CLIENT_BUILDER.comment("How many should be the max number able to exist at once? -1 = unlimited")
+                .define("maxRagdolls", -1);
 
-        RAGDOLL_LIFE = CLIENT_BUILDER.comment("How many")
-                .define("ragdollLife ", 30);
+        CONFIG_RAGDOLL_LIFE = CLIENT_BUILDER.comment("How many")
+                .define("ragdollLife", 30);
 
         CLIENT_BUILDER.pop();
 
         RAGDOLL_CONFIG = CLIENT_BUILDER.build();
     }
 
-    /*@SubscribeEvent
+    public static void loadVariables() {
+        MAX_RAGDOLLS = CONFIG_MAX_RAGDOLLS.get();
+        RAGDOLL_LIFE = CONFIG_MAX_RAGDOLLS.get();
+    }
+
+    @SubscribeEvent
     public static void onLoad(final ModConfig.Loading configEvent) {
-        // add things when needed
+        loadVariables();
     }
 
     @SubscribeEvent
     public static void onReload(final ModConfig.Reloading configEvent) {
-        // add things when needed
-    }*/
+        loadVariables();
+    }
 }
